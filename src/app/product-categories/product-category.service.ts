160fa931eafd23165c102c9fe483923869ecf55a
Anurag <ab7324@truman.edu>
2019-10-02 10:08:36 -0400

    Module for Combining Streams

--- src/app/product-categories/product-category.service.ts
+++ src/app/product-categories/product-category.service.ts
@@ -4,12 +4,19 @@ import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 
 import { ProductCategory } from './product-category';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductCategoryService {
   private productCategoriesUrl = 'api/productCategories';
+
+  productCategories$ = this.http.get<ProductCategory[]>(this.productCategoriesUrl)
+  .pipe(
+    tap(data => console.log('categories', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
 
   constructor(private http: HttpClient) { }
 
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,7 +13,7 @@ export class ProductListComponent {
   errorMessage = '';
   categories;
 
-  products$ = this.productService.products$
+  products$ = this.productService.productsWithCategory$
   .pipe(
     catchError(err => {
       this.errorMessage = err;
